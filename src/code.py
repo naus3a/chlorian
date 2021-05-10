0cc8074648c582ca58462aa40394a6d542332ea8
naus3a <naus3a@gmail.com>
2021-05-10 20:39:21 +0200

    page and midi control

--- src/code.py
+++ src/code.py
@@ -2,9 +2,15 @@ import board
 import digitalio
 import time
 import adafruit_dotstar as dotstar
+import adafruit_debouncer as debouncer
 import usb_midi
 import adafruit_midi
+from adafruit_midi.control_change import ControlChange
 import gc
+
+#########
+# led
+#########
 
 redLed = digitalio.DigitalInOut(board.D13)
 redLed.direction = digitalio.Direction.OUTPUT
@@ -12,13 +18,31 @@ redLed.value = False
 
 rgbLed = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.2)
 
-midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=0)
+def setRgbLed(_r, _g, _b, _bri):
+    rgbLed[0] = (_r, _g, _b)
+    rgbLed.brightness = _bri
 
-pages = []
-curPage = -1
+def setRgbLedFromColor(_c):
+    setRgbLed(_c[0], _c[1], _c[2], 0.2)
+
+def setRgbLedForPage(_page):
+    setRgbLedFromColor(_page[0])
 
 def makeColor(_r, _g, _b):
     return [_r, _b, _b]
+
+#########
+# midi
+#########
+
+midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=0)
+
+def sendMidiMsg(_msg):
+    nFields = len(_msg)
+    if nFields > 0:
+        if _msg[0] == 0:
+            if nFields >= 3:
+                midi.send(ControlChange(_msg[1], _msg[2]))
 
 def makeMidiMsgFromString(_s):
     m = []
@@ -33,8 +57,32 @@ def makeMidiMsgFromString(_s):
     return m
 
 #########
+# buttons
+#########
+
+def makeButton(_pin, _col, _name):
+    return [
+        digitalio.DigitalInOut(_pin),
+        _name,
+        _col
+    ]
+
+btns = [
+    makeButton(board.D3, makeColor(255,0,0), "lx"),
+    makeButton(board.D1, makeColor(0,255,0), "rx")
+]
+
+for i in range(len(btns)):
+    btns[i][0].direction = digitalio.Direction.INPUT
+    btns[i][0].pull = digitalio.Pull.UP
+    btns[i].append(debouncer.Debouncer(btns[i][0]))
+
+#########
 # config
 #########
+
+pages = []
+curPage = -1
 
 def makePageFromCsv(_csv):
     global pages
@@ -68,32 +116,25 @@ def loadConfig():
     f.close()
     loadPage(0)
 
-#########
-# led
-#########
-
-def setRgbLed(_r, _g, _b, _bri):
-    rgbLed[0] = (_r, _g, _b)
-    rgbLed.brightness = _bri
-
-def setRgbLedFromColor(_c):
-    setRgbLed(_c[0], _c[1], _c[2], 0.2)
-
-def setRgbLedForPage(_page):
-    setRgbLedFromColor(_page[0])
-
-#########
-# midi
-#########
-
-def sendMidiMsg(_msg):
-    nFields = len(_msg)
-    if nFields > 0:
-        if _msg[0] == 0:
-            if nFields >= 3:
-                midi.send(ControlChange(_msg[1], _msg[2]))
 
 gc.collect()
 loadConfig()
 while True:
-    time.sleep(0.5)
+    for i in range(len(btns)):
+        deb = btns[i][3]
+        deb.update()
+        if deb.fell:
+            setRgbLedFromColor(btns[i][2])
+        if deb.rose:
+            if deb.last_duration >= 1:
+                if i==0:
+                    curPage -= 1
+                    if curPage <0:
+                        curPage = len(pages)-1
+                else:
+                    curPage += 1
+                    if curPage >= len(pages):
+                        curPage = 0
+            else:
+                sendMidiMsg(pages[curPage][1+i])
+            setRgbLedForPage(pages[curPage])
